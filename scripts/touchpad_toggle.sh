afa8f7b783f54bcc8a3101535c25a6f5d5046ca0
pallav <pallav@archbook.com>
2024-04-18 16:46:11 +0300

    fixed script, now working

--- scripts/touchpad_toggle.sh
+++ scripts/touchpad_toggle.sh
@@ -1,5 +1,9 @@
 declare -a STATES=(1 0)
-DEVICE=$(xinput list --name-only | grep Touchpad)
+
+#DEVICE=$(xinput list --name-only | grep Touchpad)
+# 09.04.2024 when disabled, device name starts with ~
+
+DEVICE=$(xinput list | grep Touchpad | sed 's/.*id=\([0-9]*\).*/\1/')
 STATE=$(xinput list-props "$DEVICE" | grep 'Device Enabled' | sed 's/^.*:[ \t]*//')
 if [[ "$STATE" == 1 ]]; then
     notify-send "Touchpad off"
