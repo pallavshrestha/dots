601715c8c4e4378754d3e12a12fdaea54ac8877a
pallav <pallav@archbook.com>
2024-09-02 10:59:50 +0300

    added greasemonkey_scripts for darkmode exception

---
+++ qutebrowser/greasemonkey/darkmodeskip.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @match https://*.google.com/*
+// @match https://letterboxd.com/*
+// ==/UserScript==
+
+const meta = document.createElement('meta');
+meta.name = "color-scheme";
+meta.content = "dark light";
+document.head.appendChild(meta);
