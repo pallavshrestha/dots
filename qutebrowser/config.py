1ae25b45cec4553781d7ef52ca51f46cda6c6627
pallavshrestha <pallav.shrestha@ik.me>
2025-05-08 11:49:45 +0300

    updates

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -45,7 +45,11 @@ from qutebrowser.api import interceptor
 def filter_yt(info: interceptor.Request):
     """Block the given request if necessary."""
     url = info.request_url
-    if url.host() == "www.youtube.com" and url.path() == "/get_video_info" and "&adformat=" in url.query():
+    if (
+        url.host() == "www.youtube.com"
+        and url.path() == "/get_video_info"
+        and "&adformat=" in url.query()
+    ):
         info.block()
 
 
