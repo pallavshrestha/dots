76f1a5f64254dde68b094549951224032640e4a0
pallavshrestha <pallav.shrestha@ik.me>
2026-01-13 14:47:57 +0200

    updated niri configs, fixed sleep then hibernate on idle, updated refresh script

--- niri/scripts/refresh.sh
+++ niri/scripts/refresh.sh
@@ -4,10 +4,11 @@ swaybg -i "$(find ~/.config/wallpaper/ -type f |shuf -n 1)" &
 killall waybar  
 waybar &
 
-dconf write /org/gnome/gesktop/interface/color-scheme '"prefer-dark"'
+dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
 gsettings set org.gnome.desktop.interface gtk-theme 'nordic'
 gsettings set org.gnome.desktop.interface font-name 'Iosevka Nerd Font 12'
 gsettings set org.gnome.desktop.interface icon-theme 'Qogir-Dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-Dark'
 gsettings set org.gnome.desktop.interface cursor-size 28
 
+env = QT_QPA_PLATFORMTHEME,qt6ct # for Qt apps - breaks on KDE apps. Use "kde" instead as themeing manager.
