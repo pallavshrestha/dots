28062473207067b7396f5add96a779d5c040249c
pallavshrestha <pallav.shrestha@ik.me>
2026-03-25 09:13:24 +0200

    updated cookies whitelist, disabled nord.js

--- qutebrowser/cookies-whitelist.py
+++ qutebrowser/cookies-whitelist.py
@@ -1,6 +1,7 @@
 config.set("content.cookies.accept", "all", "*://*.google.com/*")
 config.set("content.cookies.accept", "all", "*://*.reddit.com/*")
 config.set("content.cookies.accept", "all", "*://*.lemmy.world/*")
+config.set("content.cookies.accept", "all", "*://lemmy.world/*")
 config.set("content.cookies.accept", "all", "*://web.telegram.org/*")
 config.set("content.cookies.accept", "all", "*://web.whatsapp.com/*")
 config.set("content.cookies.accept", "all", "*://www.facebook.com/*")
